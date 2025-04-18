23dd832b0f36252853f5cf9d80d1063e8e56dd06
tisha134 <an0018198@gmail.com>
2025-04-18 11:22:47 +0500

    part1

--- JAVASCRIPT-COURSE/scripts/12-todo-list.js
+++ JAVASCRIPT-COURSE/scripts/12-todo-list.js
@@ -11,8 +11,8 @@ renderTodoList();
 function renderTodoList() {
   let todoListHTML = '';
 
-  for (let i = 0; i < todoList.length; i++) {
-    const todoObject = todoList[i];
+  todoList.forEach(function (todoObject, index)
+  {
     //const name = todoObject.name;
     //const dueDate = todoObject.dueDate;
     const { name, dueDate } = todoObject;
@@ -20,13 +20,12 @@ function renderTodoList() {
       <div>${name}</div>
       <div>${dueDate}</div>
       <button onclick="
-        todoList.splice(${i}, 1);
+        todoList.splice(${index}, 1);
         renderTodoList();
       " class="delete-todo-button">Delete</button> 
     `;
     todoListHTML += html;
-  }
-
+  });
   document.querySelector('.js-todo-list')
     .innerHTML = todoListHTML;
 }
