c41992897e77e3c480a80a72a449a000ba8dc85c
tisha134 <an0018198@gmail.com>
2025-04-21 19:34:00 +0500

    complete

--- JAVASCRIPT-COURSE/scripts/12-rock-paper-scissors.js
+++ JAVASCRIPT-COURSE/scripts/12-rock-paper-scissors.js
@@ -21,8 +21,7 @@ function autoPlay ()
 {
   if(!isAutoPlaying)
   {
-     intervalId = setInterval(function()
-    {
+     intervalId = setInterval(() =>{
       const playerMove = pickComputerMove();
       playGame(playerMove);
     }, 1000);
@@ -32,8 +31,45 @@ function autoPlay ()
       isAutoPlaying = false;
     }
   }
-  
- 
+  //we are suppose to give a function to event listener
+ document.querySelector('.js-rock-button').addEventListener('click', () =>{
+  playGame('rock');
+ });
+
+ document.querySelector('.js-paper-button')
+ .addEventListener('click', () => {
+  playGame('paper');
+ });
+
+ document.querySelector('.js-scissors-button')
+ .addEventListener('click', () => {
+  playGame('scissors');
+ });
+
+ document.querySelector('.js-auto-play')
+ .addEventListener('click', () => {
+  autoPlay();
+ });
+
+ document.querySelector('.js-reset-button')
+ .addEventListener('click', () => {
+  score.wins = 0;
+      score.losses = 0;
+      score.ties = 0;
+      localStorage.removeItem('score');
+      updateScoreElement();
+ });
+
+
+ document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'r') {
+    playGame('rock');
+  } else if (event.key === 'p') {
+    playGame('paper');
+  } else if (event.key === 's'){
+    playGame('scissors');
+  }
+});
 
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
--- JAVASCRIPT-COURSE/scripts/12-todo-list.js
+++ JAVASCRIPT-COURSE/scripts/12-todo-list.js
@@ -11,24 +11,33 @@ renderTodoList();
 function renderTodoList() {
   let todoListHTML = '';
 
-  todoList.forEach(function (todoObject, index)
-  {
+  todoList.forEach((todoObject, index) => {
     //const name = todoObject.name;
     //const dueDate = todoObject.dueDate;
     const { name, dueDate } = todoObject;
     const html = `
       <div>${name}</div>
       <div>${dueDate}</div>
-      <button onclick="
-        todoList.splice(${index}, 1);
-        renderTodoList();
-      " class="delete-todo-button">Delete</button> 
+      <button class="delete-todo-button js-delete-todo-btn">Delete</button> 
     `;
     todoListHTML += html;
   });
   document.querySelector('.js-todo-list')
     .innerHTML = todoListHTML;
+
+    document.querySelectorAll('.js-delete-todo-btn')
+    .forEach((deleteButton, index) => {
+      deleteButton.addEventListener('click', () => {
+        todoList.splice(index, 1);
+        renderTodoList();
+      });
+    });
 }
+
+document.querySelector('.js-add-todo-btn')
+.addEventListener('click', () => {
+  addTodo();
+})
 
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
