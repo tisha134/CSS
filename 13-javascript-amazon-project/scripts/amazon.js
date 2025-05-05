b495fef50f0f1a31d8f4016bbdf88aeb35ff4005
tisha134 <an0018198@gmail.com>
2025-05-05 20:03:46 +0500

    copied code

--- 13-javascript-amazon-project/scripts/amazon.js
+++ 13-javascript-amazon-project/scripts/amazon.js
@@ -1,11 +1,11 @@
-import {cart, addToCart} from'../data/cart.js';
-import { products } from '../data/products.js';
+import {cart, addToCart} from '../data/cart.js';
+import {products} from '../data/products.js';
+import {formatCurrency} from './utils/money.js';
 
-
-let productsHtml = '';
+let productsHTML = '';
 
 products.forEach((product) => {
-  productsHtml +=`
+  productsHTML += `
     <div class="product-container">
       <div class="product-image-container">
         <img class="product-image"
@@ -25,7 +25,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-price">
-        $${(product.priceCents / 100).toFixed(2)}
+        $${formatCurrency(product.priceCents)}
       </div>
 
       <div class="product-quantity-container">
@@ -51,31 +51,31 @@ products.forEach((product) => {
       </div>
 
       <button class="add-to-cart-button button-primary js-add-to-cart"
-      data-product-id = "${product.id}">
+      data-product-id="${product.id}">
         Add to Cart
       </button>
     </div>
-  `
-},);
+  `;
+});
 
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-document.querySelector('.js-products-grid').innerHTML = productsHtml;
-
-function updateCartQuantity () {
+function updateCartQuantity() {
   let cartQuantity = 0;
+
   cart.forEach((cartItem) => {
     cartQuantity += cartItem.quantity;
   });
 
   document.querySelector('.js-cart-quantity')
-   .innerHTML = cartQuantity;
+    .innerHTML = cartQuantity;
 }
+
 document.querySelectorAll('.js-add-to-cart')
- .forEach((button) => {
-  button.addEventListener('click', () => {
-    const productId = button.dataset.productId;
-    addToCart (productId);
-    updateCartQuantity();
-    
-  });   
- });
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+      addToCart(productId);
+      updateCartQuantity();
+    });
+  });
