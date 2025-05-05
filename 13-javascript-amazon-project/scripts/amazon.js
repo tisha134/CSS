5ae9e98285eccbc804b2e9fdda3e4cbf6f46c404
tisha134 <an0018198@gmail.com>
2025-05-06 00:58:22 +0500

    make update interactive copied

--- 13-javascript-amazon-project/scripts/amazon.js
+++ 13-javascript-amazon-project/scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart, addToCart} from '../data/cart.js';
+import {cart, addToCart, calculateCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
@@ -61,15 +61,25 @@ products.forEach((product) => {
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 function updateCartQuantity() {
-  let cartQuantity = 0;
-
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
+  const cartQuantity = calculateCartQuantity();
 
   document.querySelector('.js-cart-quantity')
     .innerHTML = cartQuantity;
 }
+
+function updateCartQuantityDOM() {
+  const cartQuantity = calculateCartQuantity();
+  const cartQuantityElement = document.querySelector('.js-cart-quantity');
+  
+  if (cartQuantity > 0) {
+  cartQuantityElement.innerHTML = cartQuantity;
+  } else {
+  cartQuantityElement.innerHTML = '';
+  
+  }
+  }
+
+
 
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
