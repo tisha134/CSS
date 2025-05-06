643e6cf0ec828c0dfc72e419729b0ba58a5b0e31
tisha134 <an0018198@gmail.com>
2025-05-06 21:38:58 +0500

    export

--- 13-javascript-amazon-project/scripts/checkout.js
+++ 13-javascript-amazon-project/scripts/checkout.js
@@ -6,6 +6,13 @@ import {
 } from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
+import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
+import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
+
+hello();
+const today = dayjs();
+const deliveryDate = today.add(7, 'days');
+console.log(deliveryDate.format('dddd, MMMM D'));
 
 let cartSummaryHTML = '';
 
